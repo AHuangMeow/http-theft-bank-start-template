bd9721e9eac5f5a1b752cb7e0dbe7d77d0e60f4f
AHuangMeow <ahuangmeow@qq.com>
2025-11-13 21:25:04 +0800

    feat: passed checkpoint 4

--- checkpoint4/main.go
+++ checkpoint4/main.go
@@ -5,4 +5,24 @@ import (
 )
 
 func main() {
+	passport := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiQUh1YW5nTWVvdyIsImlhdCI6MTc2Mjk5NDkwMywibmJmIjoxNzYyOTk0OTAzfQ.t3uPobRQKbBE0xqafESebOCSXNfk4gvG8bHKXQfnSro"
+
+	request, err := httptool.NewRequest(
+		httptool.GETMETHOD,
+		"https://gtainmuxi.muxixyz.com/api/v1/muxi/backend/computer/examination",
+		"",
+		httptool.DEFAULT,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	request.SetHeader("Passport", passport)
+
+	response, err := request.SendRequest()
+	if err != nil {
+		panic(err)
+	}
+
+	response.ShowBody()
 }
