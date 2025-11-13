216a9d241098568e4fb86115ceb65306cd96246e
AHuangMeow <ahuangmeow@qq.com>
2025-11-13 17:11:45 +0800

    feat: passed checkpoint 3

--- checkpoint3/main.go
+++ checkpoint3/main.go
@@ -5,4 +5,41 @@ import (
 )
 
 func main() {
+	request, err := httptool.NewRequest(
+		httptool.GETMETHOD,
+		"http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/iris_sample",
+		"",
+		httptool.DEFAULT,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	request.SetHeader("Passport", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiQUh1YW5nTWVvdyIsImlhdCI6MTc2Mjk5NDkwMywibmJmIjoxNzYyOTk0OTAzfQ.t3uPobRQKbBE0xqafESebOCSXNfk4gvG8bHKXQfnSro")
+
+	response, err := request.SendRequest()
+	if err != nil {
+		panic(err)
+	}
+
+	response.Save("./iris.png")
+
+	request, err = httptool.NewRequest(
+		httptool.POSTMETHOD,
+		"http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/bank/iris_recognition_gate",
+		"./iris.png",
+		httptool.FILE,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	request.SetHeader("Passport", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiQUh1YW5nTWVvdyIsImlhdCI6MTc2Mjk5NDkwMywibmJmIjoxNzYyOTk0OTAzfQ.t3uPobRQKbBE0xqafESebOCSXNfk4gvG8bHKXQfnSro")
+
+	response, err = request.SendRequest()
+	if err != nil {
+		panic(err)
+	}
+
+	response.ShowBody()
 }
