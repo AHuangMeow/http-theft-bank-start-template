065012c39bd25bb65b1a746917c98e52449cc298
AHuangMeow <ahuangmeow@qq.com>
2025-11-13 16:46:26 +0800

    feat: passed checkpoint 2

--- checkpoint2/main.go
+++ checkpoint2/main.go
@@ -1,8 +1,32 @@
 package main
 
 import (
+	"github.com/AHuangMeow/hacker-support/encrypt"
 	"github.com/AHuangMeow/hacker-support/httptool"
 )
 
 func main() {
+	encrypted, err := encrypt.AESEncryptOutInBase64([]byte("for {go func(){time.Sleep(1*time.Hour)}()}"), []byte("MuxiStudio203304"))
+	if err != nil {
+		panic(err)
+	}
+
+	request, err := httptool.NewRequest(
+		httptool.PUTMETHOD,
+		"http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/bank/gate",
+		string(encrypted),
+		httptool.DEFAULT,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	request.SetHeader("Passport", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiQUh1YW5nTWVvdyIsImlhdCI6MTc2Mjk5NDkwMywibmJmIjoxNzYyOTk0OTAzfQ.t3uPobRQKbBE0xqafESebOCSXNfk4gvG8bHKXQfnSro")
+
+	response, err := request.SendRequest()
+	if err != nil {
+		panic(err)
+	}
+
+	response.ShowBody()
 }
